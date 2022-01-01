159fffba8aa3f692127f3007caa6c6fb52b4e44c
Tranh <dongquoctranh@gmail.com>
2022-01-01 20:40:47 +0700

    Init react project

---
+++ react_learning/frontend/src/components/ToDoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "@atlaskit/button"
+import Todo from "./Todo"
+
+
+export default function ToDoList() {
+    return (
+        <>
+            <Todo />
+            <Todo />
+            <Todo />
+        </>
+    )
+}
