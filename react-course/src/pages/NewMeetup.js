73fcd9a6f70ef384f5e4060695d860fb5a79b444
Tranh Dong <dongquoctranh@gmail.com>
2022-01-14 16:19:27 +0700

    Meetup App

---
+++ react-course/src/pages/NewMeetup.js
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+
+import NewMeetupForm from '../components/meetups/NewMeetupForm'
+
+
+function NewMeetupPage() {
+    const navigate = useNavigate();
+
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-course-meetups-24e75-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/');
+        });
+
+    }
+
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+}
+
+
+export default NewMeetupPage;
