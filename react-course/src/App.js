f7c6a048ec184f367586ef248ecfb97e59ecdfae
Tranh Dong <dongquoctranh@gmail.com>
2022-01-14 08:28:03 +0700

    A first app

---
+++ react-course/src/App.js
@@ -0,0 +1,15 @@
+import Todo from './components/Todo';
+
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Beat" />
+      <Todo text="XYZ" />
+    </div>
+  );
+}
+
+export default App;
